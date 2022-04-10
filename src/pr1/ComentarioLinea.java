8b6925bd9b9a6e25e73eb929182b10309dcdf474
Veronika Polonchak <polonchakveronika@gmail.com>
2022-04-10 13:42:34 +0200

    primer commit

---
+++ src/pr1/ComentarioLinea.java
@@ -0,0 +1,13 @@
+package pr1;
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		
+		//Mostrar por consola Strings
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
